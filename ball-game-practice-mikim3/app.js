edd2e12ab2c999cb4b68534af38ea34649a0c849
minsukim <minsu980107@naver.com>
2023-05-29 18:43:55 +0900

    공튀기기 게임 만들기

--- ball-game-practice-mikim3/app.js
+++ ball-game-practice-mikim3/app.js
@@ -1,3 +1,7 @@
+import {
+    Ball
+} from './ball.js';
+
 class App {
     constructor() {
         this.canvas = document.createElement('canvas');
@@ -7,6 +11,8 @@ class App {
 
         window.addEventListener('resize', this.resize.bind(this), false);
         this.resize();
+
+        this.ball = new Ball(this.stageWidth, this.stageHeight, 60, 15);
 
         window.requestAnimationFrame(this.animate.bind(this));
     }
@@ -21,6 +27,12 @@ class App {
     }
 
     animate(t) {
+        window.requestAnimationFrame(this.animate.bind(this));
+
+        this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
+
+        this.ball.draw(this.ctx, this.stageWidth, this.stageHeight);
+        // this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight); 
 
     }
 }
